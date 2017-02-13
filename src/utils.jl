d7c7c2e692d913eb3bf75c967e15966d9eba711b
Brendan McVeigh <mcveigh.brendan@gmail.com>
2017-02-12 19:49:20 -0500

    add utils.jl file for misc functions, currently contains methods for converting times into strings

---
+++ src/utils.jl
@@ -0,0 +1,41 @@
+"""
+Converts a Base.Dates.XX format into a string in HH:MM:SS format.
+"""
+function duration_to_string(duration::Base.Dates.Hour)
+    duration = convert(Int64, duration)
+    return string(duration)
+end
+
+function duration_to_string(duration::Base.Dates.Minute)
+    duration = convert(Int64, duration)
+    minutes = duration % 60
+    hours = (duration - minutes) / 60
+    minutes = convert(Int64, minutes)
+    hours = convert(Int64, hours)
+    return string(hours, ":", minutes)
+end
+
+function duration_to_string(duration::Base.Dates.Second)
+    duration = convert(Int64, duration)
+    seconds = duration % 60
+    duration = (duration - seconds) / 60
+    minutes = duration % 60
+    hours = (duration - minutes) / 60
+    seconds = convert(Int64, seconds)
+    minutes = convert(Int64, minutes)
+    hours = convert(Int64, hours)
+    return string(hours, ":", minutes, ":", seconds)
+end
+
+function duration_to_string(duration::Base.Dates.Millisecond)
+    duration = convert(Int64, duration)
+    duration = floor(duration / 1000)
+    seconds = duration % 60
+    duration = (duration - seconds) / 60
+    minutes = duration % 60
+    hours = (duration - minutes) / 60
+    seconds = convert(Int64, seconds)
+    minutes = convert(Int64, minutes)
+    hours = convert(Int64, hours)
+    return string(hours, ":", minutes, ":", seconds)
+end
