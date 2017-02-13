dd03c69ce19639387387f3fa1c3b199cc686325f
Brendan McVeigh <mcveigh.brendan@gmail.com>
2017-02-12 19:37:33 -0500

    sort distances from smallest to largest (single measure) or rank distances by one of several metrics (multiple meausres)

---
+++ src/rank_distances.jl
@@ -0,0 +1,60 @@
+"""
+Function to return the permutation of the computed distances that will order from
+smallest to largest.  In this case of multiple distance functions a cross dimention
+comparison is made by summing the ranks for each dimention and then ordering the
+sum from smallest to largest
+
+# Arguments
+* `distances::Array{Float64, 1}, SharedArray{Float64, 1}, Array{Array{Float64, 1}, 1}, SharedArray{Array{Float64, 1}, 1}`:
+    vector of distances or vector of distance vectors
+
+# Value
+permutation of indicies so such that distances are ordered from smallest to largest
+"""
+function perm_distance(distances::Array{Float64, 1})
+    return sortperm(distances)
+end
+
+
+function sumrank_distance(distances::Array{Float64, 2})
+    n, m = size(distances)
+    rankArray = Array{Float64}(n, m)
+    for jj in 1:m
+        rankArray[:, JJ] sortperm(distances[:, jj])
+    end
+    totals = vec(sum(rankArray, 2))
+    return sortperm(totals)
+end
+
+
+"""
+Function to return the permutation of the computed distances that will order from
+smallest to largest.  In this case of multiple distance functions a cross dimention
+comparison is made by ranking each observation across each dimention and then assigning
+each observation the maximum across all dimentions.  Ties are dealt with by using
+the default value for sortperm().
+
+# Arguments
+* `distances::Array{Array{Float64, 1}, 1}, SharedArray{Array{Float64, 1}, 1}`:
+    vector of distances or vector of distance vectors
+
+# Value
+permutation of indicies so such that distances are ordered from smallest to largest
+"""
+function maxrank_distance(distances::Array{Float64, 2})
+    n, m = size(distances)
+    rankArray = Array{Float64}(n, m)
+    for jj in 1:m
+        rankArray[:, JJ] sortperm(distances[:, jj])
+    end
+    totals = vec(maximum(rankArray, 2))
+    return sortperm(totals)
+end
+
+function normrank_distance(distances::Array{Float64, 2})
+    distmin = minimum(distances, 1)
+    diststd = std(distances, 1)
+    scaledDistances = (distances .- distmin) ./ diststd
+    totals = vec(sum(scaledDistances))
+    return sortperm(totals)
+end
