ced4a83b76969ec8c38865c3a15c4ebdbd21d791
Brendan McVeigh <mcveigh.brendan@gmail.com>
2017-02-16 00:23:29 -0500

    improve documentation for functions in rank_distances.jl

--- src/rank_distances.jl
+++ src/rank_distances.jl
@@ -1,4 +1,16 @@
 """
+Identical to `identity()` used for 1-d distances that do not need to be transformed
+before being sorted
+"""
+function identiy_distance{G <: Real}(distances::Array{G, 1})
+    return identity(distances)
+end
+
+function identiy_distance{G <: Real}(distances::Array{G, 2})
+    return identity(distances)
+end
+
+"""
 Function to return the permutation of the computed distances that will order from
 smallest to largest.  In this case of multiple distance functions a cross dimention
 comparison is made by summing the ranks for each dimention and then ordering the
@@ -11,12 +23,7 @@ sum from smallest to largest
 # Value
 permutation of indicies so such that distances are ordered from smallest to largest
 """
-function identiy_distance(distances::Array{Float64, 1})
-    return identity(distances)
-end
-
-
-function sumrank_distance(distances::Array{Float64, 2})
+function sumrank_distance{G <: Real}(distances::Array{G, 2})
     n, m = size(distances)
     rankArray = Array{Float64}(n, m)
     for jj in 1:m
@@ -28,20 +35,22 @@ end
 
 
 """
-Function to return the permutation of the computed distances that will order from
-smallest to largest.  In this case of multiple distance functions a cross dimention
-comparison is made by ranking each observation across each dimention and then assigning
-each observation the maximum across all dimentions.  Ties are dealt with by using
-the default value for sortperm().
+Maximum rank seen in each row when ranks are compute for columns
+`maxrank_distance(distances)`
+A cross dimention comparison is made by ranking each observation across each
+dimension and then assigning each observation the maximum across all dimentions.
 
 # Arguments
-* `distances::Array{Array{Float64, 1}, 1}, SharedArray{Array{Float64, 1}, 1}`:
-    vector of distances or vector of distance vectors
+* `distances::Array{Float64, 2}`: vector of distances or vector of distance vectors
 
 # Value
-permutation of indicies so such that distances are ordered from smallest to largest
+Vector containing the maximum rank across each row
+
+# Examples
+`distances = rand(10,3)`
+`maxrank_distance(distances)`
 """
-function maxrank_distance(distances::Array{Float64, 2})
+function maxrank_distance{G <: Real}(distances::Array{G, 2})
     n, m = size(distances)
     rankArray = Array{Float64}(n, m)
     for jj in 1:m
@@ -51,7 +60,21 @@ function maxrank_distance(distances::Array{Float64, 2})
     return totals
 end
 
-function normrank_distance(distances::Array{Float64, 2})
+"""
+Normalized compound distance
+`normrank_distance(distances)`
+
+# Arguments
+* `distances::Array{Float64, 2}`: vector of distances or vector of distance vectors
+
+# Value
+Vector containing sum across rows of normalized distances
+
+# Examples
+`distances = rand(10,3)`
+`maxrank_distance(distances)`
+"""
+function normrank_distance{G <: Real}(distances::Array{G, 2})
     distmin = minimum(distances, 1)
     diststd = std(distances, 1)
     scaledDistances = (distances .- distmin) ./ diststd
