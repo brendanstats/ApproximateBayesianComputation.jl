7fb002152c5e728ed8ba065fabf817b160a8f0c9
Brendan McVeigh <mcveigh.brendan@gmail.com>
2017-02-22 13:04:47 -0500

    additional documentation and function for processing lists of AbcPmcStep objects

--- src/post_processing.jl
+++ src/post_processing.jl
@@ -7,14 +7,11 @@
 #{A <: SingleMeasureAbcPmc}
 #{A <: MultiMeasureAbcPmc
 
-#=
+
 """
-Takes array of AbcPmcStep and returns an array with the first column the cumulative
-samples drawn across steps and the second column the acceptance threshold used totalsamples_thresholds(x)
-"""
-=#
-"""
-test
+`totalsamples_thresholds(Array{AbcPmcStep, 1})`
+
+Takes array of AbcPmcStep and returns a tuple `(labels, [samples, thresholds])` where row `i` corresponds to the `ith` entry of the input Array.  The first column will list the total number of samples drawn in the corresponding step while subsequent columns will list threshold information.  In the case of a `MultiMeasureAbcPmc` array labels are given as 'threshold1', 'threshold2', ...
 """
 function totalsamples_thresholds{A <: SingleMeasureAbcPmc}(x::Array{A, 1})
     T = eltype(x[1].threshold)
@@ -37,3 +34,37 @@ function totalsamples_thresholds{A <: MultiMeasureAbcPmc}(x::Array{A, 1})
     end
     return (["samples"; map(x -> string("threshold", x), 1:size(thresholds, 2))], [cumsum(totalsamples) thresholds])
 end
+
+"""
+`particles_weights(Array{AbcPmcStep, 1})`
+
+Takes array of AbcPmcStep and returns a tuple `(labels, [step, particles, weights])` where row `step` corresponds to the entry of the input Array.  In the case of a `MultivariateAbcPmc` array labels are given as 'particle1', 'particle2', ....  The last column contains the weights associated with the particles.
+"""
+function particles_weights{A <: UnivariateAbcPmc}(x::Array{A, 1})
+    n = length(x)
+    m = length(x[1].particles)
+    out = Array{promote_type(eltype(x[1].particles), eltype(x[1].weights), Int64)}(n * m, 3)
+    out[:, 1] = repeat(1:n, inner = m)
+    for ii in 1:n
+        rng = range((ii - 1) * m + 1, 1, m)
+        out[rng, 2] = x[ii].particles
+        out[rng, 3] = x[ii].weights.values
+    end
+    return (["step", "particle", "weight"], out)
+end
+
+function particles_weights{A <: MultivariateAbcPmc}(x::Array{A, 1})
+    n = length(x)
+    m, k = size(x[1].particles)
+    out = Array{promote_type(eltype(x[1].particles), eltype(x[1].weights), Int64)}(n * m, k + 2)
+    out[:, 1] = repeat(1:n, inner = m)
+    for ii in 1:n
+        rng = range((ii - 1) * m + 1, 1, m)
+        out[rng, 2:(k + 1)] = x[ii].particles
+        out[rng, k + 2] = x[ii].weights.values
+    end
+    return (["step"; map(x -> string("particle", x), 1:k); "weight"], out)
+end
+
+#workspace()
+#whos()
